bf114a5c6410823695f7ff5647833bb89f08fff6
Dmitri José Nunes Ferreira <dmitrijnferreira@gmail.com>
2025-09-16 22:12:58 -0300

    Adicionando o primeiro desafio Ruby do Curso Ruby para iniciantes

---
+++ desafio-ruby01.rb
@@ -0,0 +1,8 @@
+puts "Bem vindo a biblioteca LivrosDM"
+print "Por favor digite seu nome: "
+nome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.chomp
+
+puts "Acesso Liberado para #{nome} que tem #{idade} anos de idade"

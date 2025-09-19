f4d316f3280224e38d228cc4e5149bffe5776fa6
Dmitri José Nunes Ferreira <dmitrijnferreira@gmail.com>
2025-09-18 23:31:42 -0300

    Adicionando o segundo desafio de Ruby, Desafio da Calculadora

---
+++ desafio-ruby02.rb
@@ -0,0 +1,35 @@
+opcao = nil
+while opcao != 0
+  puts "1 Soma
+        2 Subtração
+        3 Multiplicação
+        4 Divisão
+  Quais operações você quer fazer?: "
+  opcao = gets.chomp.to_i
+
+  puts "Digite o primeiro número: "
+  num1 = gets.chomp.to_i
+
+  puts "Digite o segundo número: "
+  num2 = gets.chomp.to_i
+  
+  case opcao
+    when 1
+    puts "#{num1} + #{num2} = #{num1+num2}"
+    
+    when 2
+      puts "#{num1} - #{num2} = #{num1-num2}"
+
+    when 3
+      puts "#{num1} x #{num2} = #{num1*num2}"
+
+    when 4
+      puts "#{num1} % #{num2} = #{num1/num2}"
+
+    when 0
+      puts "Encerrado"
+
+    else
+      puts "Opção inválida"
+  end
+end
